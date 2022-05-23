02ffefa8bea71a286c1adb2e7d3f29ce861cbda4
Kundan Singh <kundasin591998@outlook.com>
2022-05-23 11:11:32 +0530

    checkpoint 11

--- scripts/common.js
+++ scripts/common.js
@@ -13,3 +13,44 @@ function closeLogin(e) {
 function handleContactUs(e) {
   getInTouchModal.style.display = "block";
 }
+
+
+/*--------------------------------------------------------------------*/
+
+function store(){
+  var inputUsername=document.getElementById("username");
+  var inputPassword=document.getElementById("password");
+  localStorage.setItem("username",inputUsername.value);
+  localStorage.setItem("password",inputPassword.value);
+};
+
+function userLogin(){
+  store();
+  let username=document.getElementById("username").value;
+  let password=document.getElementById("password").value;
+  sessionStorage.username = 'admin';
+  sessionStorage.isLogin = true;
+  if(username==='admin' && password === 'admin'){
+     confirm('Successfully loggedin');
+
+   var btn = document.getElementById("loginM");
+   btn.value = "logout"; 
+   btn.innerText="LOGOUT";
+   document.getElementById("payNow").disabled=false;
+   
+  }
+
+}
+
+function logoutFunc(){
+    var btn1=document.getElementById("loginM");    
+    if(btn1.value=="logout"){
+     btn1.value = "login"; 
+     btn1.innerText="LOGIN";
+     localStorage.clear();
+     document.getElementById("payNow").disabled=true;
+    }
+}
+
+/*--------------------------------------------------------------------*/
+
